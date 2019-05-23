6297ecba8c8859906b5b3a4586c72406fdf6a970
Kumar <00507468@DS.INDIANOIL.IN>
2019-05-23 20:22:42 +0530

    Chapter 5, after adding authetication check + page check bef authenticate before checki opening page

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
 
 from app.forms import LoginForm
 
@@ -22,8 +24,25 @@ def index():
 
 @app.route('/login',methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form=LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user=User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid Username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index')
     return render_template('login.html',title='Sign-in',form=form)
+
+
+@app.logout('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/checklogin')
+@login_required
+def login_check():
+    return index()
